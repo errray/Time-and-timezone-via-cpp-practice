0b57423bb26e71052327a45cdce7ed167b862030
errray <94083732+errray@users.noreply.github.com>
2024-10-20 00:04:28 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+int main()
+{
+
+    TimeZone timezone_1(20,30,7);
+    timezone_1.change_time_zone(-5);
+
+
+/*
+
+    Time time_2 = time_1;
+    time_2.display_time();
+
+    time_1.increment_minute();
+    time_1.display_time();
+    time_1.decrement_minute();
+    time_1.display_time();
+
+
+    time_1.set_minute(20);
+    time_1.get_minute();
+    time_1.display_time();
+
+    return 0;
+*/
+
+}
